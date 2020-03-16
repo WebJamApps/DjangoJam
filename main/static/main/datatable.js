f3a6e839897bb05635673b5843856bab5e9b95f6
Joshua Sherman <jsherman@valcom.com>
2020-03-16 13:50:22 -0400

    edit and delete buttons

--- main/static/main/datatable.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var oTable = $('.datatable').dataTable({
-    "searching": true,
-    "processing": true,
-    "serverSide": true,
-    "ajax": "{% url 'order_list_json' %}"
-  });
-});
